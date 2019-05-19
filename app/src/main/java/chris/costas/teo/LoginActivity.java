b3136de05ad24ec4ff424fce8724692c30b575ab
teotsi <teotsi@gmail.com>
2019-05-19 19:27:35 +0300

    Changes are now being saved properly

--- app/src/main/java/chris/costas/teo/LoginActivity.java
+++ app/src/main/java/chris/costas/teo/LoginActivity.java
@@ -3,21 +3,19 @@ package chris.costas.teo;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
@@ -29,11 +27,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.services.Service;
+
 import static android.Manifest.permission.READ_CONTACTS;
-import model.services.AccountService;
+import static model.services.AccountService.emailIsValid;
+import static model.services.AccountService.login;
+import static model.services.AccountService.passwordIsValid;
 
 /**
  * A login screen that offers login via email/password.
@@ -58,6 +64,13 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try {
+            File f = null;
+            Service.CompanyReader(this.getAssets().open("dataset/Companies.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Service.CompanyReader("Companies.txt"));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
@@ -185,11 +198,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
     private boolean isEmailValid(String email) {
-        return AccountService.emailIsValid(email);
+        return emailIsValid(email);
     }
 
     private boolean isPasswordValid(String password) {
-        return AccountService.passwordIsValid(password);
+        return passwordIsValid(password);
     }
 
     /**
@@ -298,25 +311,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
-            return true;
+            return login(mEmail, mPassword);
         }
 
         @Override
@@ -325,6 +320,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
+
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
