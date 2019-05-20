ca4745e14fda451771a1e3f5d575cf2d9d556af2
Costas Xarhoulacos <p3160121@aueb.gr>
2019-05-20 12:18:45 +0300

    activities add

---
+++ app/src/main/java/chris/costas/teo/CreateNewAccount.java
@@ -0,0 +1,25 @@
+package chris.costas.teo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CreateNewAccount extends AppCompatActivity {
+
+    EditText CompName, CompEmail, Password, CompAddress, IBAN, RentalRange;
+    Button CreateAccountBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_account);
+
+        CompName = (EditText)findViewById(R.id.CompanyNameTextField);
+        CompEmail = (EditText)findViewById(R.id.EmailTextField);
+        Password = (EditText)findViewById(R.id.PasswordTextField);
+        CompAddress = (EditText)findViewById(R.id.CompanyAddressTextField);
+        IBAN = (EditText)findViewById(R.id.IBANTextField);
+        RentalRange = (EditText)findViewById(R.id.RentalRangeTextField);
+    }
+}
