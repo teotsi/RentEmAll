46be44eca707375b1303db723cb50df22646293e
teotsi <teotsi@gmail.com>
2019-05-22 22:08:06 +0300

    Progress on Datepicker

--- app/src/main/java/chris/costas/teo/CalendarDatePickerDialog.java
+++ app/src/main/java/chris/costas/teo/CalendarDatePickerDialog.java
@@ -3,11 +3,57 @@ package chris.costas.teo;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.widget.DatePicker;
 
 import androidx.fragment.app.DialogFragment;
 
-public class CalendarDatePickerDialog extends DialogFragment{
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            return new DatePickerDialog(getActivity());
-        }
+import java.time.LocalDate;
+import java.util.Calendar;
+
+public class CalendarDatePickerDialog extends DialogFragment {
+    private String tag;
+    private DatePickerDialog.OnDateSetListener dateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    if (tag.equals("start")) {
+                        int viewMonth = view.getMonth();
+                        int viewDay = view.getDayOfMonth();
+
+                        String monthString = String.valueOf(viewMonth);
+                        if (viewMonth < 10) {
+                            monthString = 0 + String.valueOf(month);
+                        }
+                        String dayString = String.valueOf(viewDay);
+                        if (viewDay < 10) {
+                            dayString = 0 + String.valueOf(viewDay);
+                        }
+                        SearchActivity.setStartDate(LocalDate.parse(view.getYear() + "-" +
+                                monthString + "-" + dayString));
+                    } else if (tag.equals("end")) {
+                        int viewMonth = view.getMonth();
+                        int viewDay = view.getDayOfMonth();
+
+                        String monthString = String.valueOf(viewMonth);
+                        if (viewMonth < 10) {
+                            monthString = 0 + String.valueOf(month);
+                        }
+                        String dayString = String.valueOf(viewDay);
+                        if (viewDay < 10) {
+                            dayString = 0 + String.valueOf(viewDay);
+                        }
+                        SearchActivity.setEndDate(LocalDate.parse(view.getYear() + "-" +
+                                monthString + "-" + dayString));
+                    }
+                }
+            };
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        tag = this.getTag();
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        return new DatePickerDialog(getActivity(), dateSetListener, year, month, day);
+    }
 }
--- app/src/main/java/chris/costas/teo/SearchActivity.java
+++ app/src/main/java/chris/costas/teo/SearchActivity.java
@@ -3,47 +3,55 @@ package chris.costas.teo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
-import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.time.LocalDate;
 
 public class SearchActivity extends AppCompatActivity {
-    private LocalDate startDate;
-    private LocalDate endDate;
+    private static LocalDate startDate;
+    private static LocalDate endDate;
+    private static TextView startText;
+    private static TextView endText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         Button startButton = (Button) findViewById(R.id.start_date_pick);
         Button endButton = (Button) findViewById(R.id.end_date_pick);
+        startText = (TextView) findViewById(R.id.start_date);
+        endText = (TextView) findViewById(R.id.end_date);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showDatePicker();
+                showDatePicker("start");
             }
         });
         endButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showDatePicker();
+                showDatePicker("end");
             }
         });
     }
 
-    private void showDatePicker(){
+    private void showDatePicker(String type){
         DialogFragment datepicker = new CalendarDatePickerDialog();
-        datepicker.show(getSupportFragmentManager(), "date picker");
+        datepicker.show(getSupportFragmentManager(), type);
 
     }
 
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
+    public static void setStartDate(LocalDate startDate) {
+        SearchActivity.startDate = startDate;
+        System.out.println("hola");
+        startText.setText(startDate.toString());
     }
 
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
+    public static void setEndDate(LocalDate endDate) {
+        SearchActivity.endDate = endDate;
+        System.out.println("hola3");
+        endText.setText(endDate.toString());
     }
 }
