5c47e2db0a52250e164c1f5e34dd0c7b15f479d6
Costas Xarhoulacos <p3160121@aueb.gr>
2019-05-27 16:19:38 +0300

    activities add

--- app/src/main/java/chris/costas/teo/Business/NewAccount/CreateNewAccountPresenter.java
+++ app/src/main/java/chris/costas/teo/Business/NewAccount/CreateNewAccountPresenter.java
@@ -1,7 +1,9 @@
 package chris.costas.teo.Business.NewAccount;
 
+import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
+import android.provider.Telephony;
 import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
@@ -11,6 +13,8 @@ import android.widget.Toast;
 import java.io.IOException;
 import java.util.List;
 
+import chris.costas.teo.R;
+import model.classes.BankAccount;
 import model.classes.CompanyAccount;
 import model.services.AccountService;
 import model.services.Service;
@@ -20,7 +24,7 @@ public class CreateNewAccountPresenter implements CreateNewAccountContract.Prese
     private CreateNewAccountContract.MvpView mView;
 
     CreateNewAccountPresenter(CreateNewAccountContract.MvpView view){
-        mView = view;
+        this.mView = view;
     }
 
     @Override
@@ -29,31 +33,125 @@ public class CreateNewAccountPresenter implements CreateNewAccountContract.Prese
     }
 
     @Override
-    public boolean DataValidation(){
-        return mView.checkDataEntered();
+    public boolean DataValidation(EditText CompAddress, EditText CompName, EditText Policy, EditText Description, EditText IBAN, EditText RentalRange, double latitude, double longitude , EditText CompEmail, EditText Password, EditText TIN){
+        boolean flag = true;
+        if(mView.isEmail(CompEmail) == false){
+            CompEmail.setError("Enter a valid email");
+            flag = false;
+        }
+        if(mView.infoExists(CompName, "name") == true){
+            CompName.setError("The company name you entered already exists!");
+            flag = false;
+        }
+        if(mView.infoExists(CompEmail, "email") == true){
+            CompEmail.setError("The email you entered already exists!");
+            flag = false;
+        }
+        if(!AccountService.passwordIsValid(Password.getText().toString())){
+            Password.setError("Password must be 8-32 characters long and contain a number, an Uppercase letter, a lowercase letter, and !@#$%^&+=");
+            flag = false;
+        }
+
+
+        if(mView.isEmpty(CompName)){
+            mView.ToastMessages("CompName");
+            flag = false;
+        }
+        if(mView.isEmpty(CompEmail)){
+            mView.ToastMessages("CompEmail");
+            flag = false;
+        }
+        if(mView.isEmpty(Password)){
+            mView.ToastMessages("Password");
+            flag = false;
+        }
+        if(mView.isEmpty(CompAddress)){
+            mView.ToastMessages("CompAddress");
+            flag = false;
+        }
+        if(mView.isEmpty(IBAN)){
+            mView.ToastMessages("IBAN");
+            flag = false;
+        }
+        if(mView.isEmpty(RentalRange)){
+            mView.ToastMessages("RentalRange");
+            flag = false;
+        }
+        if(mView.isEmpty(Policy)){
+            mView.ToastMessages("Policy");
+            flag = false;
+        }
+//        Uncomment if you want description to be necessary
+//        if(isEmpty(Description)){
+//            Toast t = Toast.makeText(this, "You must enter a company description", Toast.LENGTH_SHORT);
+//            t.show();
+//        }
+        return flag;
     }
 
     @Override
-    public boolean CheckEmpty(EditText txt){
-        return mView.isEmpty(txt);
+    public boolean CheckEmpty(EditText text){
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
     }
 
     @Override
-    public boolean CheckEmail(EditText txt){
-        return mView.isEmail(txt);
+    public boolean CheckEmail(EditText text){
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
     }
 
     @Override
     public boolean InfoExistence(EditText text, String dataType){
-        return mView.infoExists(text, dataType);
+        boolean nameFlag = false;
+        boolean emailFlag = false;
+        boolean addressFlag = false;
+        CharSequence info = text.getText().toString();
+        if(dataType.equals("name")){
+            List<CompanyAccount> companies = Service.getCompanies();
+            for (CompanyAccount c : companies){
+                if(c.getCompanyName().equals(info)){
+                    nameFlag = true;
+                    break;
+                }
+            }
+            return nameFlag;
+        }else if(dataType.equals("email")){
+            List<CompanyAccount> companies = Service.getCompanies();
+            for (CompanyAccount c : companies){
+                if(c.getEmail().equals(info)){
+                    emailFlag = true;
+                    break;
+                }
+            }
+            return emailFlag;
+        }return false;
     }
 
     @Override
-    public boolean LongLat() throws IOException{
-        return mView.SetCoordinates();
+    public boolean LongLat(Context con, List<Address> addressList, EditText CompAddress, double latitude, double longitude) throws IOException{
+        boolean coordflag = false;
+        Geocoder gc = new Geocoder(con);
+        addressList = gc.getFromLocationName(CompAddress.getText().toString(), 1);
+
+        if(addressList.size() != 0){
+            Address add = addressList.get(0);
+
+            latitude = add.getLatitude();
+            longitude = add.getLongitude();
+            coordflag = true;
+        }else{
+            mView.ToastMessages("LongLat");
+        }
+        return  coordflag;
     }
 
-
+    @Override
+    public void newAccount(EditText CompName, EditText Policy, EditText Description, EditText IBAN, EditText RentalRange, double latitude, double longitude , EditText CompEmail, EditText Password, EditText TIN){
+        BankAccount newBA = new BankAccount(CompName.getText().toString(), IBAN.getText().toString(), 0);
+        //TODO ADD new User to dataset here
+        AccountService.register(CompName.getText().toString(), Policy.getText().toString(), Description.getText().toString(), Float.parseFloat(RentalRange.getText().toString()), latitude, longitude, CompEmail.getText().toString(), Password.getText().toString(), TIN.getText().toString(),newBA);
+    }
 
 }
 
